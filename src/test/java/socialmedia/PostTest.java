f9e6111747931ab1d6e73e9971ec9495fbbb0129
Fredrik Jonsén <frejo105@student.liu.se>
2017-03-29 15:23:28 +0200

    Fixed issues with caused tests to not work with maven

--- src/test/java/socialmedia/PostTest.java
+++ src/test/java/socialmedia/PostTest.java
@@ -1,6 +1,5 @@
 package socialmedia;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -17,22 +16,18 @@ import static org.junit.jupiter.api.Assertions.*;
     This whole test class is mostly to add code coverage.
     The only functions with any actual logic is getEditTime
 */
-public class PostTest {
+class PostTest {
 
-    private Post post = null;
-
-    static {
-        assert(true);
-    }
+    Post post = null;
 
     @BeforeEach
-    public void beforeEach() {
-        post = new Post() {};
+    void init() {
+        this.post = new Post() {};
     }
 
     @Test
     @DisplayName("should set and get post body text unchanged")
-    public void testText() {
+    void testText() {
         String text = "Some body text";
         post.setText(text);
         assertEquals(text, post.getText());
@@ -40,7 +35,7 @@ public class PostTest {
 
     @Test
     @DisplayName("should set and get post creation time unchanged")
-    public void testCreationTime() {
+    void testCreationTime() {
         ZonedDateTime time = ZonedDateTime.now();
         post.setCreationTime(time);
         assertEquals(time, post.getCreationTime());
@@ -48,7 +43,7 @@ public class PostTest {
 
     @Test
     @DisplayName("should set and get post edit time unchanged")
-    public void testEditTime() {
+    void testEditTime() {
         ZonedDateTime time = ZonedDateTime.now();
         post.setEditTime(time);
         assertEquals(time, post.getEditTime());
@@ -56,15 +51,16 @@ public class PostTest {
 
     @Test
     @DisplayName("should return creation time when edit is null")
-    public void testNullEditTime() {
+    void testNullEditTime() {
         ZonedDateTime time = ZonedDateTime.now();
         post.setCreationTime(time);
+        post.setEditTime(null);
         assertEquals(time, post.getEditTime());
     }
 
     @Test
     @DisplayName("should set and get id unchanged")
-    public void testId() {
+    void testId() {
         String id = "someid";
         post.setId(id);
         assertEquals(id, post.getId());
@@ -72,7 +68,7 @@ public class PostTest {
 
     @Test
     @DisplayName("should set and get author unchanged")
-    public void testAuthor() {
+    void testAuthor() {
         User user = new User();
         int age = 20;
         user.setAge(age);
@@ -82,7 +78,7 @@ public class PostTest {
 
     @Test
     @DisplayName("should set and get share count unchanged")
-    public void testShareCount() {
+    void testShareCount() {
         int count = 10;
         post.setSharedCount(count);
         assertEquals(count, post.getSharedCount());
@@ -90,7 +86,7 @@ public class PostTest {
 
     @Test
     @DisplayName("should set and get post language unchanged")
-    public void testLanguage() {
+    void testLanguage() {
         String language = "Swedish";
         post.setLanguage(language);
         assertEquals(language, post.getLanguage());
@@ -98,7 +94,7 @@ public class PostTest {
 
     @Test
     @DisplayName("should set and get receiving users unchanged")
-    public void testToUsers() {
+    void testToUsers() {
         User first = new User();
         first.setAge(10);
         User second = new User();
@@ -115,7 +111,7 @@ public class PostTest {
 
     @Test
     @DisplayName("should set and get post tags unchanged")
-    public void testTags() {
+    void testTags() {
         List<String> expected = new ArrayList<>();
         expected.add("first");
         expected.add("second");
@@ -129,7 +125,7 @@ public class PostTest {
 
     @Test
     @DisplayName("should set and get post permalink unchanged")
-    public void testPermalink() throws MalformedURLException {
+    void testPermalink() throws MalformedURLException {
         URL url = new URL("https://example.com/endpoint#resource?param=value");
         post.setPermalink(url);
         assertEquals(url, post.getPermalink());
@@ -137,7 +133,7 @@ public class PostTest {
 
     @Test
     @DisplayName("should set and get location name unchanged")
-    public void testLocationName() {
+    void testLocationName() {
         String location = "Stockholm";
         post.setLocationName(location);
         assertEquals(location, post.getLocationName());
