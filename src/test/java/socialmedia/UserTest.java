bf2dd47451f5b827a49564100aeac1eddb3c3997
Alexander Stolpe <alexander.stolpe@gmail.com>
2017-03-31 10:26:44 +0200

    TwitterUser added. User, FacebookUser and TwitterUser classes done, documentation not finnished

--- src/test/java/socialmedia/UserTest.java
+++ src/test/java/socialmedia/UserTest.java
@@ -1,4 +1,13 @@
 package socialmedia;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserTest {
+
+    private User user = null;
+
+
 }
