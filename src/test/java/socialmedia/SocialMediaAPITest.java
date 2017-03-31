1f7fe6e920f8963296ceb8fb293171a00efd846f
Fredrik Jonsén <frejo105@student.liu.se>
2017-03-31 14:19:07 +0200

    Added tests for SocialMediaAPI

--- src/test/java/socialmedia/SocialMediaAPITest.java
+++ src/test/java/socialmedia/SocialMediaAPITest.java
@@ -1,14 +1,38 @@
 package socialmedia;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SocialMediaAPITest {
 
+    SocialMediaAPI sm;
+    PrintStream ps;
+
+    @BeforeEach
+    void init() {
+        sm = Mockito.mock(SocialMediaAPI.class, Mockito.CALLS_REAL_METHODS);
+        ps = Mockito.mock(PrintStream.class);
+        sm.setDebugStream(ps);
+    }
+
     @Test
-    @DisplayName("Herro")
-    public void testT() {
-        assertEquals("This", "This");
+    @DisplayName("debug should print the given message")
+    void testDebug() {
+        sm.debug("something");
+        Mockito.verify(ps).println("something");
+    }
+
+    @Test
+    @DisplayName("debugf should forward the given objects")
+    void testDebugf() {
+        sm.debugf("something", "one", "two");
+        Mockito.verify(ps).printf("something", "one", "two");
     }
 }
