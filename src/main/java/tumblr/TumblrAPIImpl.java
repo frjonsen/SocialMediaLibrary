0c2cea28009a4d44d9ddb8cd17094b7e07b7483a
Alexander Stolpe <alexander.stolpe@gmail.com>
2017-05-05 14:12:22 +0200

    Added tumblrPostConversion class to structure code more

--- src/main/java/tumblr/TumblrAPIImpl.java
+++ src/main/java/tumblr/TumblrAPIImpl.java
@@ -61,8 +61,8 @@ public class TumblrAPIImpl extends TumblrAPI {
         try {
             long lId = Long.parseLong(id);
             Post post = libraryInstance.blogPost(blogName, lId);
-            return jumblrPostConversion(post);
-        } catch(JumblrException|NumberFormatException e) {
+            return TumblrPostConversion.jumblrPostConversion(post);
+        } catch(JumblrException|NumberFormatException|MalformedURLException e) {
             debug(e);
             throw new TumblrAPIException(e.getMessage());
         }
@@ -118,7 +118,16 @@ public class TumblrAPIImpl extends TumblrAPI {
     public List<TumblrPost> getPostFeed(String id) {
         String blogId = (id == null || id.equals(SELF_ID)) ? activeBlog : id;
         List<Post> posts = libraryInstance.blogPosts(blogId);
-        return posts.stream().map(this::jumblrPostConversion).collect(Collectors.toList());
+        try {
+            List<TumblrPost> returnList = new ArrayList<>();
+            for(Post post: posts) {
+                returnList.add(TumblrPostConversion.jumblrPostConversion(post));
+            }
+            return returnList;
+        } catch(MalformedURLException mue) {
+            debug(mue);
+            throw new TumblrAPIException(mue.getMessage());
+        }
 
     }
 
@@ -190,241 +199,6 @@ public class TumblrAPIImpl extends TumblrAPI {
             throw new TumblrAPIException("No blog with id \"" + id + "\"");
         libraryInstance.unfollow(id);
         return true; // jumblr doesn't return whether it worked. Assume it worked as long as blog exists.
-    }
-
-    private TumblrPost jumblrPostConversion(Post jumblrPost) {
-        if(jumblrPost == null) {
-            return null;
-        }
-        TumblrPost post = new TumblrPost();
-        post.setLiked(jumblrPost.isLiked());
-
-        if(jumblrPost.getAuthorId() != null) {
-            TumblrUser user = new TumblrUser(USER);
-            user.setId(jumblrPost.getAuthorId());
-            post.setAuthor(user);
-        }
-        Date date = new Date();
-        date.setTime(jumblrPost.getTimestamp() * 1000);
-        post.setCreationTime(date);
-        post.setId(String.valueOf(jumblrPost.getId()));
-        try {
-            post.setPermalink(new URL(jumblrPost.getPostUrl()));
-        } catch (MalformedURLException me) {
-            debug(me);
-            throw new TumblrAPIException(me.getMessage());
-        }
-
-        long noteCount = jumblrPost.getNoteCount();
-        if(jumblrPost.getNotes() != null) {
-            long reblogCount = jumblrPost.getNotes()
-                    .stream()
-                    .filter(note -> "reblog".equals(note.getType()))
-                    .count();
-            post.setSharedCount((int) reblogCount);
-            post.setLikeCount((int) (noteCount - reblogCount));
-        }
-        post.setTags(jumblrPost.getTags());
-        if (jumblrPost.getRebloggedFromName() != null) {
-            TumblrUser rebloggedFrom = new TumblrUser(BLOG);
-            rebloggedFrom.setName(jumblrPost.getRebloggedFromName());
-            post.setTo(Arrays.asList(rebloggedFrom));
-        }
-        post.setReblogKey(jumblrPost.getReblogKey());
-
-        return postTypeSpecificConversion(post, jumblrPost);
-    }
-
-    private TumblrPost postTypeSpecificConversion(TumblrPost post, Post jumblrPost) {
-        switch(jumblrPost.getType().toUpperCase()) {
-            case "TEXT":
-                return textPostConverter(post, jumblrPost);
-            case "PHOTO":
-                return photoPostConverter(post, jumblrPost);
-            case "QUOTE":
-                return quotePostConverter(post, jumblrPost);
-            case "LINK":
-                return linkPostConverter(post, jumblrPost);
-            case "CHAT":
-                return chatPostConverter(post, jumblrPost);
-            case "AUDIO":
-                return audioPostConverter(post, jumblrPost);
-            case "VIDEO":
-                return videoPostConverter(post, jumblrPost);
-            case "ANSWER":
-                return answerPostConverter(post, jumblrPost);
-            default:
-                post.setType(UNKNOWN);
-                return post;
-        }
-    }
-
-    private TumblrPost photoPostConverter(TumblrPost tumblrPost, Post jumblrPost) {
-        PhotoPost post = (PhotoPost)jumblrPost;
-        tumblrPost.setType(IMAGE);
-
-        String body = post.getCaption();
-        String text = "";
-        if(!SocialMediaUtil.isNullOrWhitespace(body)) {
-            text += body;
-        }
-        String urls = post.getPhotos()
-                .stream()
-                .map(photo -> photo.getOriginalSize().getUrl())
-                .reduce("", (a, b) -> a + "\n" + "\"" + b +"\"");
-        if(!SocialMediaUtil.isNullOrWhitespace(urls)) {
-            text += urls;
-        }
-        tumblrPost.setText(text);
-        return tumblrPost;
-    }
-
-    private TumblrPost quotePostConverter(TumblrPost tumblrPost, Post jumblrPost) {
-        tumblrPost.setType(QUOTE);
-        QuotePost quotePost = (QuotePost)jumblrPost;
-        String source = quotePost.getSource();
-        String body = quotePost.getText();
-        String text = "";
-        if(!SocialMediaUtil.isNullOrWhitespace(body)) {
-            text += body;
-        }
-        if(!SocialMediaUtil.isNullOrWhitespace(source)) {
-            text += "\n\nsource: \"" + quotePost.getSource() + "\"";
-        }
-
-        tumblrPost.setText(text);
-
-        return tumblrPost;
-    }
-
-    private TumblrPost textPostConverter(TumblrPost tumblrPost, Post jumblrPost) {
-        tumblrPost.setType(TEXT);
-        TextPost textPost = (TextPost)jumblrPost;
-        String text = "";
-        String title = textPost.getTitle();
-        String body = textPost.getBody();
-        if(!SocialMediaUtil.isNullOrWhitespace(title)) {
-            text += title + "\n\n";
-        }
-        if(!SocialMediaUtil.isNullOrWhitespace(body)) {
-            text += body;
-        }
-        tumblrPost.setText(text);
-
-        return tumblrPost;
-    }
-
-    private TumblrPost linkPostConverter(TumblrPost tumblrPost, Post jumblrPost) {
-        tumblrPost.setType(LINK);
-        LinkPost linkPost = (LinkPost)jumblrPost;
-        String text = "";
-        String title = linkPost.getTitle();
-        String description = linkPost.getDescription();
-        String url = linkPost.getLinkUrl();
-
-        if(!SocialMediaUtil.isNullOrWhitespace(title)) {
-            text += title;
-        }
-        if(!SocialMediaUtil.isNullOrWhitespace(description)) {
-            text += "\n\n" + description;
-        }
-        if(!SocialMediaUtil.isNullOrWhitespace(url)) {
-            text += "\n\n" + "source: \"" + linkPost.getLinkUrl() + "\"";
-        }
-        tumblrPost.setText(text);
-
-        return tumblrPost;
-    }
-
-    private TumblrPost chatPostConverter(TumblrPost tumblrPost, Post jumblrPost) {
-        tumblrPost.setType(CHAT);
-        ChatPost chatPost = (ChatPost)jumblrPost;
-        String text = "";
-        String title = chatPost.getTitle();
-        String body = chatPost.getBody();
-        if(!SocialMediaUtil.isNullOrWhitespace(title)) {
-            text += title;
-        }
-        if(!SocialMediaUtil.isNullOrWhitespace(body)) {
-            text += "\n\n" + body;
-        }
-
-        tumblrPost.setText(text);
-
-        return tumblrPost;
-    }
-
-    private TumblrPost audioPostConverter(TumblrPost tumblrPost, Post jumblrPost) {
-        tumblrPost.setType(AUDIO);
-        AudioPost audioPost = (AudioPost)jumblrPost;
-        String text = "";
-        String title = audioPost.getSourceTitle();
-        String caption = audioPost.getCaption();
-        String source = audioPost.getSourceUrl();
-        if(!SocialMediaUtil.isNullOrWhitespace(title)) {
-            text += title;
-        }
-        if(!SocialMediaUtil.isNullOrWhitespace(caption)) {
-            text += "\n\n" + caption;
-        }
-        if(!SocialMediaUtil.isNullOrWhitespace(source)) {
-            text += "\n\n" + source;
-        }
-        tumblrPost.setText(text);
-
-        return tumblrPost;
-    }
-
-    private TumblrPost videoPostConverter(TumblrPost tumblrPost, Post jumblrPost) {
-        tumblrPost.setType(VIDEO);
-        VideoPost videoPost = (VideoPost)jumblrPost;
-        String caption = videoPost.getCaption();
-        String thumbnail = videoPost.getThumbnailUrl();
-        String text = "";
-
-        if(!SocialMediaUtil.isNullOrWhitespace(caption)) {
-            text += caption;
-        }
-        if(!SocialMediaUtil.isNullOrWhitespace(thumbnail)) {
-            text += "url: \"" + thumbnail + "\"";
-        }
-
-        tumblrPost.setText(text);
-
-        return tumblrPost;
-    }
-
-    private TumblrPost answerPostConverter(TumblrPost tumblrPost, Post jumblrPost) {
-        tumblrPost.setType(ANSWER);
-        AnswerPost answerPost = (AnswerPost)jumblrPost;
-        String askingName = answerPost.getAskingName();
-
-        TumblrUser questionUser = new TumblrUser(BLOG);
-        if(tumblrPost.getTo() == null){
-            tumblrPost.setTo(Arrays.asList(questionUser));
-        } else {
-            List<TumblrUser> toList = (List<TumblrUser>)tumblrPost.getTo();
-            toList.add(questionUser);
-            tumblrPost.setTo(toList);
-        }
-
-        String text = "";
-        String question = answerPost.getQuestion();
-        String answer = answerPost.getAnswer();
-
-        if(!SocialMediaUtil.isNullOrWhitespace(askingName)) {
-            text += askingName;
-            questionUser.setName(askingName);
-        }
-        if(!SocialMediaUtil.isNullOrWhitespace(question)) {
-            text += "\n\n" + question;
-        }
-        if(!SocialMediaUtil.isNullOrWhitespace(answer)) {
-            text += "\n\n" + answer;
-        }
-        tumblrPost.setText(text);
-
-        return tumblrPost;
     }
 
     TumblrAPIImpl(JumblrClient client) {
